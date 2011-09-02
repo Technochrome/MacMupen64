f7c6c7bca024239919c1945889a4aae5d83ba8ae
Rovolo <rovolo@s213-103-214-15.cust.tele2.se>
2011-09-02 14:20:46 +0200

    Initial Commit

---
+++ MacMupen64/MacMupen64AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  MacMupen64AppDelegate.h
+//  MacMupen64
+//
+//  Created by Johnny on 9/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MacMupen64AppDelegate : NSObject <NSApplicationDelegate> {
+	NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MacMupen64/MacMupen64AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  MacMupen64AppDelegate.m
+//  MacMupen64
+//
+//  Created by Johnny on 9/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MacMupen64AppDelegate.h"
+
+@implementation MacMupen64AppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
